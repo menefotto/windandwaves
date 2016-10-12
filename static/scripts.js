8e2a7ba1a204dd7240d5249fab9fbd70a56242ab
wind85 <locci.carlo.1985@gmail.com>
2016-10-12 07:49:43 +0200

    "errors"

--- static/scripts.js
+++ static/scripts.js
@@ -7,59 +7,65 @@ const config = {
 };
 firebase.initializeApp(config);
 
-Errors = function(text) {
-    var err = document.getElementById('err');
-    const errbtn = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
-    const message = '<div class="alert alert-danger" role="alert">' + errbtn + text + '</div>'
-    err.innerHTML = message
-}
-
-
-$("#signup").click(function() {
-    const user = $("#username").val();
-    const pass = $("#password").val();
-    const auth = firebase.auth();
-
-    console.log(user + pass);
-    auth.createUserWithEmailAndPassword(user, pass).catch(function(error) {
-        var errorCode = error.code;
-        errorMsg = error.message;
-
-        Errors(errorMsg);
-    });
-});
-
-$("#login").click(function() {
-    const user = $("#username").val();
-    const pass = $("#password").val();
-    const auth = firebase.auth();
-    alert(user + pass);
-
-    auth.signInWithEmailAndPassword(user, pass).catch(function(error) {
-        var errorCode = error.code;
-        errorMsg = error.message;
-
-        Errors(errorMsg);
-    });
-});
-
-$("#reset").click(function() {
-    const user = $("#username").val();
-    const auth = firebase.auth();
-    alert("user" + user);
-    auth.sendPasswordResetEmail(user).catch(function(error) {
-        var errorCode = error.code;
-        errorMsg = error.message;
-
-        Errors(errorMsg);
-    });
-});
-
-
 firebase.auth().onAuthStateChanged(function(firebaseUser) {
     if (firebaseUser) {
         console.log("logged in");
     } else {
         console.log("not logged in");
+    }
+});
+
+PrintError = function(text) {
+    const errbtn = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+    const message = '<div class="alert alert-danger" role="alert">' + errbtn + text + '</div>';
+    const err = document.getElementById('err');
+
+    localStorage.setItem("lastError", message);
+    err.innerHTML = message;
+}
+
+GetLastError = function(err) {
+    return localStorage.getItem("lastError");
+}
+
+$(function() {
+    $("#signup").click(function() {
+        const user = $("#username").val();
+        const pass = $("#password").val();
+        const auth = firebase.auth();
+
+        auth.createUserWithEmailAndPassword(user, pass).catch(function(error) {
+
+            PrintError(error.message);
+        });
+    });
+
+    $("#login").click(function() {
+        const user = $("#username").val();
+        const pass = $("#password").val();
+        const auth = firebase.auth();
+
+        auth.signInWithEmailAndPassword(user, pass).catch(function(error) {
+
+            PrintError(error.message);
+        });
+    });
+
+    $("#reset").click(function() {
+        const user = $("#username").val();
+        const auth = firebase.auth();
+
+        auth.sendPasswordResetEmail(user).catch(function(error) {
+
+            PrintError(error.message);
+        });
+    });
+
+    lastError = $("#err").text();
+    console.log(lastError);
+    if (lastError != " ") {
+        localStorage.setItem("lastError", "");
+    } else {
+        PrintError(GetLastError());
     }
 });
