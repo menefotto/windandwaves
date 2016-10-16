6987bf304a6eda0fbfee0d6e4ec5a925ae7158e2
wind85 <locci.carlo.1985@gmail.com>
2016-10-16 10:51:49 +0200

    fixed errors pop up

--- static/scripts.js
+++ static/scripts.js
@@ -7,7 +7,7 @@ const config = {
 }
 firebase.initializeApp(config)
 
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     console.log(user.email + ' logged in')
     $('#nickname').append(user.email)
@@ -16,98 +16,93 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 })
 
-var pushError = function(error) {
-  $('#errp').append('<strong>Ops something went wrong: </strong>' + error.message)
+var pushError = function (message) {
+  $('#errp').append('<strong>Ops something went wrong: </strong>' + message)
   $('#err').removeClass('hide')
 }
 
-var pushMessage = function(message) {
+var pushMessage = function (message) {
   $('#errp').append('<strong>Operation perform successfully: </strong>' + message)
   $('#err').removeClass('alert-danger')
   $('#err').addClass('alert-success')
   $('#err').removeClass('hide')
 }
 
-$(function() {
-  $('.close').click(function() {
+$(function () {
+  $('.close').click(function () {
     location.reload()
   })
 
-  $('#signup').click(function(event) {
+  $('#signup').click(function (event) {
     const user = $('#username').val()
     const pass = $('#password').val()
     const auth = firebase.auth()
 
-    auth.createUserWithEmailAndPassword(user, pass).then(function(value) {
+    auth.createUserWithEmailAndPassword(user, pass).then(function (value) {
       location.replace('loggedin')
-    }, function(error) {
-      pushError(error)
+    }, function (error) {
+      pushError(error.message)
     })
 
     return false
   })
 
-  $('#login').click(function(event) {
+  $('#login').click(function (event) {
     const user = $('#username').val()
     const pass = $('#password').val()
     const auth = firebase.auth()
 
-    auth.signInWithEmailAndPassword(user, pass).then(function(value) {
-        location.replace('loggedin')
-      },
-      function(error) {
-        pushError(error)
+    auth.signInWithEmailAndPassword(user, pass).then(function (value) {
+      location.replace('loggedin')
+    },
+      function (error) {
+        pushError(error.message)
       })
 
     return false
   })
 
-  $('#reset').click(function(event) {
+  $('#reset').click(function (event) {
     event.preventDefault()
 
     const user = $('#username').val()
     const auth = firebase.auth()
 
     var res = auth.sendPasswordResetEmail(user)
-    res.then(function() {
-      pushMessage("An email should be arriving shortly!")
-    }, function(error) {
-      pushError(error)
+    res.then(function () {
+      pushMessage('An email should be arriving shortly!')
+    }, function (error) {
+      pushError(error.message)
     })
   })
-  
-  $('#change_passwd').click(function(event) {
+
+  $('#change_passwd').click(function (event) {
     event.preventDefault()
     
-    const passwd = $('#passwd_reset').val()
-    const passwd1 = $('#passwd_reset1').val()
-    if ( passwd === passwd1) {        
-      pushError("Ops passwords must be the same!")
-      return
-    }
-    
-    var codeurl = $(location).attr("href").split("=")[2]
-    const code = codeurl.split("&")[0]
-    
-    console.log("reset code is: ", code)
+    var codeurl = $(location).attr('href').split('=')[2]
+    const code = codeurl.split('&')[0]
     
     const auth = firebase.auth()
+    
+    var passwd = $('#passwd_reset').val()
+    var passwd1 = $('#passwd_reset1').val()
+    if (passwd === passwd1) {
+      pushError('Ops passwords must be the same!')
+      return false
+    }
+
     var res = auth.confirmPasswordReset(code, passwd)
-    res.then(function() {
-        location.replace('login')
-      },
+    res.then(function () {
+      location.replace('login')
+    },
       function() {
-        pushError("Invalid reset code!")
+        pushError('Invalid reset code!')
       })
 
     return false
   })
 
-
-  $('#logout').click(function() {
+  $('#logout').click(function () {
     firebase.auth().signOut()
   })
-
-  
- 
 })
